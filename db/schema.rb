49e5a4f5cdbe5ebdb37c08321ce342756fce2437
Ivan <ivan@Rocios-MacBook-Pro.local>
2018-06-28 18:17:54 -0700

    deleted a duplicated table in the database and added the resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_235618) do
+ActiveRecord::Schema.define(version: 2018_06_29_010937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,17 @@ ActiveRecord::Schema.define(version: 2018_06_28_235618) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "skills", force: :cascade do |t|
+  create_table "portfolios", force: :cascade do |t|
     t.string "title"
-    t.integer "percent_utilized"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "skils", force: :cascade do |t|
+  create_table "skills", force: :cascade do |t|
     t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
